891ae6e2d404c36706a430c216818d92a4d99b9d
xMoogle <32349043+xMoogle@users.noreply.github.com>
2022-06-14 00:06:55 -0300

    Add files via upload

--- farmacia/src/main/java/com/farmaciagen/farmacia/repository/ProdutoRepository.java
+++ farmacia/src/main/java/com/farmaciagen/farmacia/repository/ProdutoRepository.java
@@ -3,9 +3,11 @@ package com.farmaciagen.farmacia.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.farmaciagen.farmacia.model.Produto;
 
+@Repository
 public interface ProdutoRepository extends JpaRepository<Produto, Long>
 {
  public List<Produto> findAllByNomeContainingIgnoreCase (String produto);
